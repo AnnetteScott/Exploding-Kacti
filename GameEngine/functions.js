d57f26895c9ebd0f79d05b029259171ab5e5e5f2
NotNatural21 <89242396+NotNatural21@users.noreply.github.com>
2021-10-21 12:19:14 +1300

    Update functions.js

--- GameEngine/functions.js
+++ GameEngine/functions.js
@@ -65,3 +65,29 @@ function generateID(){
   for(var i = 0; i < 10; i++){id += chars[parseInt(Math.random() * chars.length)];}
   return id;
 }
+
+function moveObject(){
+  document.getElementById("tunnel").animate([
+    { transform: 'translate3D(0, 0, 0)' }, 
+    { transform: 'translate3D(0, -300px, 0)' }
+  ], {
+    duration: 1000,
+    iterations: Infinity
+  })
+
+  var aliceTumbling = [
+    { transform: 'rotate(0) translate3D(-50%, -50%, 0', color: '#000' }, 
+    { color: '#431236', offset: 0.3},
+    { transform: 'rotate(360deg) translate3D(-50%, -50%, 0)', color: '#000' }
+  ];
+
+  var aliceTiming = {
+    duration: 3000,
+    iterations: Infinity
+  }
+
+  document.getElementById("alice").animate(
+    aliceTumbling, 
+    aliceTiming
+  )
+}

f3a867828297ed8d108595047f8ac4347d01a7ec
NotNatural21 <89242396+NotNatural21@users.noreply.github.com>
2021-10-26 17:08:33 +1300

    Update functions.js
    THIS IS NOT NEEDED

--- GameEngine/functions.js
+++ GameEngine/functions.js
@@ -105,34 +105,6 @@ function generateID(){
   return id;
 }
 
-
-function moveObject(){
-  document.getElementById("tunnel").animate([
-    { transform: 'translate3D(0, 0, 0)' }, 
-    { transform: 'translate3D(0, -300px, 0)' }
-  ], {
-    duration: 1000,
-    iterations: Infinity
-  })
-
-  var aliceTumbling = [
-    { transform: 'rotate(0) translate3D(-50%, -50%, 0', color: '#000' }, 
-    { color: '#431236', offset: 0.3},
-    { transform: 'rotate(360deg) translate3D(-50%, -50%, 0)', color: '#000' }
-  ];
-
-  var aliceTiming = {
-    duration: 3000,
-    iterations: Infinity
-  }
-
-  document.getElementById("alice").animate(
-    aliceTumbling, 
-    aliceTiming
-  )
-}
-
-
 function updateCursorPosition(e){
   cursor_position.x = e.clientX;
   cursor_position.y = e.clientY;
@@ -150,3 +122,4 @@ function crosshair(){
   crosshair_elem.style.left = cursor_position.x - 8 + "px";
   crosshair_elem.style.top = cursor_position.y - 8 + "px";
 }
+

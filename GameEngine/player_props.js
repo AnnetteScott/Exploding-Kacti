bba2c9259d4630464df8e3f4b9ed0ab3825895dd
NotNatural21 <89242396+NotNatural21@users.noreply.github.com>
2021-10-27 08:53:01 +1300

    Update player_props.js

--- GameEngine/player_props.js
+++ GameEngine/player_props.js
@@ -8,3 +8,13 @@ var heart_item = {
   'health_regen': 10,
   'spawn_chance': 20
 }
+
+
+var difficulty = [
+  {'level': 'L1', 'maxCacti': 10, 'scoreThreshold': 10},
+  {'level': 'L2', 'maxCacti': 20, 'scoreThreshold': 20},
+  {'level': 'L3', 'maxCacti': 30, 'scoreThreshold': 30},
+  {'level': 'L4', 'maxCacti': 40, 'scoreThreshold': 40},
+  {'level': 'L5', 'maxCacti': 50, 'scoreThreshold': 50},
+  {'level': 'L6', 'maxCacti': 60, 'scoreThreshold': 60}
+]

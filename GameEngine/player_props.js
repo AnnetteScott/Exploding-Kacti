dc9037f47bdcb8ae5e6b256a69d1e3e6316fdf47
NotNatural21 <89242396+NotNatural21@users.noreply.github.com>
2021-10-28 09:28:43 +1300

    Update player_props.js

--- GameEngine/player_props.js
+++ GameEngine/player_props.js
@@ -13,12 +13,12 @@ var heart_item = {
   'heart_min_spawn_distance': 400
 };
 
-
+var spawn_speed = 1000
 var difficulty = [
-  {'level': 'L1', 'maxCacti': 10, 'scoreThreshold': 10},
-  {'level': 'L2', 'maxCacti': 15, 'scoreThreshold': 50},
-  {'level': 'L3', 'maxCacti': 20, 'scoreThreshold': 100},
-  {'level': 'L4', 'maxCacti': 25, 'scoreThreshold': 150},
-  {'level': 'L5', 'maxCacti': 30, 'scoreThreshold': 200},
-  {'level': 'L6', 'maxCacti': 35, 'scoreThreshold': 250}
+  {'level': 'L1', 'maxCacti': 10, 'scoreThreshold': 10, 'spawnSpeed': 1000},
+  {'level': 'L2', 'maxCacti': 15, 'scoreThreshold': 50, 'spawnSpeed': 950},
+  {'level': 'L3', 'maxCacti': 20, 'scoreThreshold': 100, 'spawnSpeed': 850},
+  {'level': 'L4', 'maxCacti': 25, 'scoreThreshold': 150, 'spawnSpeed': 800},
+  {'level': 'L5', 'maxCacti': 30, 'scoreThreshold': 200, 'spawnSpeed': 750},
+  {'level': 'L6', 'maxCacti': 35, 'scoreThreshold': 250, 'spawnSpeed': 700}
 ];

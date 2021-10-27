6fa8e88896c594fc5174b56339d5014e1d154cb8
NotNatural21 <89242396+NotNatural21@users.noreply.github.com>
2021-10-28 09:56:52 +1300

    added more difficulty

--- GameEngine/player_props.js
+++ GameEngine/player_props.js
@@ -15,10 +15,10 @@ var heart_item = {
 
 var spawn_speed = 1000
 var difficulty = [
-  {'level': 'L1', 'maxCacti': 10, 'scoreThreshold': 10, 'spawnSpeed': 1000},
-  {'level': 'L2', 'maxCacti': 15, 'scoreThreshold': 50, 'spawnSpeed': 950},
-  {'level': 'L3', 'maxCacti': 20, 'scoreThreshold': 100, 'spawnSpeed': 850},
-  {'level': 'L4', 'maxCacti': 25, 'scoreThreshold': 150, 'spawnSpeed': 800},
-  {'level': 'L5', 'maxCacti': 30, 'scoreThreshold': 200, 'spawnSpeed': 750},
-  {'level': 'L6', 'maxCacti': 35, 'scoreThreshold': 250, 'spawnSpeed': 700}
+  {'level': 'L1', 'maxCacti': 10, 'scoreThreshold': 10, 'spawnSpeed': 1000, 'cactiMovingSpeed': 1, 'fireCacti': 2},
+  {'level': 'L2', 'maxCacti': 15, 'scoreThreshold': 50, 'spawnSpeed': 950, 'cactiMovingSpeed': 1.2, 'fireCacti': 5},
+  {'level': 'L3', 'maxCacti': 20, 'scoreThreshold': 75, 'spawnSpeed': 850, 'cactiMovingSpeed': 1.4, 'fireCacti': 10},
+  {'level': 'L4', 'maxCacti': 25, 'scoreThreshold': 100, 'spawnSpeed': 800, 'cactiMovingSpeed': 1.5, 'fireCacti': 20},
+  {'level': 'L5', 'maxCacti': 30, 'scoreThreshold': 100, 'spawnSpeed': 750, 'cactiMovingSpeed': 1.6, 'fireCacti': 25},
+  {'level': 'L6', 'maxCacti': 35, 'scoreThreshold': 200, 'spawnSpeed': 600, 'cactiMovingSpeed': 1.75, 'fireCacti': 30}
 ];

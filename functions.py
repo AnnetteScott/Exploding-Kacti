0fa52680a6fd3900354ebefaccd0d4ea3292bff9
Giga <gigabyte5671@gmail.com>
2021-10-29 11:05:32 +1300

    Create functions.py

---
+++ functions.py
@@ -0,0 +1,20 @@
+import math
+
+def zeropad(string, length):
+	string = str(string)
+	pad = ""
+	for index in range(length - len(str)):
+		pad += "0"
+	return pad + string
+
+
+def getLinearDistance(p1, p2):
+  	return math.sqrt(((p2['x'] - p1['x']) * (p2['x'] - p1['x'])) + ((p2['y'] - p1['y']) * (p2['y'] - p1['y'])))
+
+
+def degToRad(deg):
+	return deg * (math.pi / 180)
+
+
+def radToDeg(rad):
+  	return rad * (180 / math.pi)

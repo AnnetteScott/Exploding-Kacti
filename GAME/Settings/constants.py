a47efdcf59d49c5d6a173141deac8d6a7760a37d
Giga <gigabyte5671@gmail.com>
2021-10-30 11:59:38 +1300

    Update constants.py

--- GAME/Settings/constants.py
+++ GAME/Settings/constants.py
@@ -3,6 +3,7 @@ import pygame
 SCREEN_HEIGHT = 720
 SCREEN_WIDTH = 1280
 SCREEN = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+SCREEN_CENTER = {'x': SCREEN_WIDTH / 2, 'y': SCREEN_HEIGHT / 2}
 
 MOUSE_POS = pygame.mouse.get_pos()
 

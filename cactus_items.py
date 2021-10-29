0bd169ec7cd4e24a6424c0e2048d2bd54c72d894
NotNatural21 <annettemorganscott@outlook.co.nz>
2021-10-29 22:42:31 +1300

    Create cactus_items.py

---
+++ cactus_items.py
@@ -0,0 +1,48 @@
+cacti_movement_speed = 1
+cacti_min_spawn_distance = 400
+max_num_of_cacti = 10
+all_cacti = {}
+cacti_types = {
+  "normal_cacti": {
+    'spawn_chance': 60,
+    'health': 10,
+    'points': 1,
+    'color': "034206"
+  },
+  "left_cacti": {
+    'spawn_chance': 15,
+    'health': 10,
+    'points': 2,
+    'color': "034206"
+  },
+  "right_cacti": {
+    'spawn_chance': 15,
+    'health': 10,
+    'points': 2,
+    'color': "034206"
+  },
+  "blob_cacti":  {
+    'spawn_chance': 10,
+    'health': 20,
+    'points': 3,
+    'color': "034206"
+  },
+  "dead_cacti": {
+    'spawn_chance': 8,
+    'health': 10,
+    'points': 4,
+    'color': "7A3400"
+  },
+  "tumbleweed": {
+    'spawn_chance': 5,
+    'health': 20,
+    'points': 4,
+    'color': "A04400"
+  },
+  "fire_cacti": {
+    'spawn_chance': 2,
+    'health': 40,
+    'points': 5,
+    'color': "FE9300"
+  }
+}

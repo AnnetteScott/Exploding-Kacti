69eb1597dddaa231feb0f8b89e496afc65595853
NotNatural21 <89242396+NotNatural21@users.noreply.github.com>
2021-10-30 15:57:15 +1300

    Update main.py
    started make them move

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 #Libraries
 import pygame
 from pygame import image
+from threading import Thread
 from pygame.constants import MOUSEBUTTONDOWN
 pygame.font.init()
 pygame.init()
 #Function Files
 from GAME.Settings import constants
+from GAME.Settings import player
+from GAME.Settings import cactus_items
 from GAME.Engine import functions
 from GAME.Engine import routines
 
@@ -22,9 +25,8 @@ pygame.display.set_caption("Exploding Kacti")
 #####################################################################################
 #-----------------------------------Draws the Game----------------------------------#
 #####################################################################################
-
-#Draws Background Image
-def draw_background():
+def draw_backdrop():
+	#Draws Background Image
 	image_dim = 500
 	BACKGROUND = pygame.image.load('GAME/Images/sand_background.png').convert_alpha()
 	BACKGROUND = pygame.transform.scale(BACKGROUND, (image_dim, image_dim))
@@ -32,43 +34,48 @@ def draw_background():
 		for x in range(0, SCREEN_WIDTH, image_dim):
 			SCREEN.blit(BACKGROUND, (x, y))
 
-#Draws pond in middle of screen
-def draw_pond():
+	#Draws pond in middle of screen
 	POND = pygame.image.load('GAME/Images/ponds/pond10.png')
 	POND = pygame.transform.scale(POND, (POND_DIM, POND_DIM))
 	pond_offsetX = SCREEN_WIDTH / 2 - POND_DIM / 2
 	pond_offsetY = SCREEN_HEIGHT / 2 - POND_DIM / 2
 	SCREEN.blit(POND, (pond_offsetX, pond_offsetY))
 
-def draw_meters():
+def draw_water_meter():
 	bar_height = 15
-	bar_width = POND_DIM
+	bar_width =  POND_DIM * (player.pond_item['water_amount'] / 100)
 	barX = SCREEN_CENTER['x'] - POND_DIM / 2 
 	barY = SCREEN_CENTER['y'] + POND_DIM / 2
-	pygame.draw.rect(SCREEN, (0, 0, 0), (barX, barY, bar_width, bar_height), 3)
-	routines.createText('Health', 15, barX + POND_DIM / 2 - 26, barY - 1)
 	barY += bar_height
-	pygame.draw.rect(SCREEN, (0, 0, 0), (barX, barY, bar_width, bar_height), 3)
-	routines.createText('Water', 15, barX + POND_DIM / 2 - 21, barY - 1)
-	# barY = barY + 20
-	# pygame.draw.rect(SCREEN, (0, 0, 0), (barX, barY, bar_width, bar_height), 3)
-	# routines.createText('Water', 15, barX - 13, barY)
+	pygame.draw.rect(SCREEN, (0, 0, 255), (barX, barY, bar_width, bar_height)) #Red Fill
+	pygame.draw.rect(SCREEN, (0, 0, 0), (barX, barY, POND_DIM, bar_height), 3) #Outline
+	routines.createText('Water', 15, barX + POND_DIM / 2 - 21, barY - 1) #Text
+	
+def draw_health_meter():
+	bar_height = 15
+	bar_width = POND_DIM * (player.player['health'] / 100)
+	barX = SCREEN_CENTER['x'] - POND_DIM / 2 
+	barY = SCREEN_CENTER['y'] + POND_DIM / 2
+	pygame.draw.rect(SCREEN, (255, 0, 0), (barX, barY, bar_width, bar_height)) #Red Fill
+	pygame.draw.rect(SCREEN, (0, 0, 0), (barX, barY, POND_DIM, bar_height), 3) #Outline
+	routines.createText('Health', 15, barX + POND_DIM / 2 - 26, barY - 1) #Text
 
-
-draw_background()
-draw_pond()
-draw_meters()
-pygame.display.update()
 #####################################################################################
 #----------------------------------------Cacti--------------------------------------#
 #####################################################################################
 
-def draw_cactus():
-	image_dim = 64
-	cactus = pygame.image.load('GAME/Images/sprites/normal_cacti.png').convert_alpha()
-	cactus = pygame.transform.scale(cactus, (image_dim, image_dim))
-	SCREEN.blit(cactus, (150, 150))
+def draw_cactus(cacti_object):
+	cacti_type = cacti_object['cacti_type']
+	pos = cacti_object['pos']
+	cactus = pygame.image.load('GAME/Images/sprites/'+ cacti_type +'.png').convert_alpha()
+	cactus = pygame.transform.scale(cactus, (cactus_items.cacti_dim, cactus_items.cacti_dim))
+	SCREEN.blit(cactus, (pos['x'], pos['y']))
 
+def check_cacti_num():
+	if len(cactus_items.all_cacti.keys()) < cactus_items.max_num_of_cacti:
+		cacti_object = routines.spawnCactus()
+		pygame.time.delay(1000)
+		draw_cactus(cacti_object)
 
 
 
@@ -77,11 +84,11 @@ def draw_cactus():
 #####################################################################################
 
 def click_events():
-	print(functions.checkObjectClick(POND_DIM, POND_DIM, SCREEN_CENTER['x'], SCREEN_CENTER['y']))
 	if functions.checkObjectClick(POND_DIM, POND_DIM, SCREEN_CENTER['x'], SCREEN_CENTER['y']):
-		draw_cactus()		
+		routines.changeWater(50)
+		draw_water_meter()
 		pygame.display.update()
-		pygame.time.delay(1000)
+		
 
 
 #PUT EVERYTHING BEFORE THIS
@@ -92,11 +99,22 @@ def click_events():
 	
 run = True
 while run:
-	pygame.time.delay(100)
+	pygame.time.delay(50)
+	draw_backdrop()
+	draw_water_meter()
+	draw_health_meter()
+	thread1 = Thread(target=check_cacti_num)
+	thread2 = Thread(target=routines.moveAllCacti())
+	thread1.start()
+	pygame.time.delay(50)
+	thread2.start()
+	
 	for event in pygame.event.get():
 		if event.type == MOUSEBUTTONDOWN:		
 			click_events()
 			run = False
+	
+	pygame.display.update()
 
 pygame.quit()
 

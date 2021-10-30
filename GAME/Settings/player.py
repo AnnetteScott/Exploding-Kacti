354828d687ecc530bc6a9ac70de76e8dd4003bca
Giga <gigabyte5671@gmail.com>
2021-10-30 22:18:03 +1300

    Update player.py

--- GAME/Settings/player.py
+++ GAME/Settings/player.py
@@ -1,3 +1,4 @@
+from . import constants
 
 player = {
   'health': 100
@@ -5,5 +6,8 @@ player = {
 
 pond_item = {
   'pond_hit_distance': 40,
-  'water_amount': 50
+  'water_amount': 50,
+  'pos': constants.SCREEN_CENTER,
+  'dim': {'width': constants.POND_DIM, 'height': constants.POND_DIM},
+  'hit_radius': 40
 }
